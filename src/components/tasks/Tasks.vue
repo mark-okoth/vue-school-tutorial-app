ad0e3335c5987afbd696a7b77755dbfa825d4189
mark odhiambo okoth <markokoth96@gmail.com>
2020-05-22 10:40:48 +0300

    task component

--- src/components/tasks/Tasks.vue
+++ src/components/tasks/Tasks.vue
@@ -1,5 +1,29 @@
 <template>
-    <div class="">
-        
+    <div class="tasks">
+        <div class="container">
+
+        </div>
     </div>
 </template>
+
+<script>
+export default {
+    data(){
+        return{
+
+        }
+    }
+}
+</script>
+
+<style scoped>
+
+.container{
+    width: 33.3vw;
+    border: 1px solid white;
+    height: 100vh;
+    border-top:none;
+    border-left:none;
+}
+
+</style>
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,15 +1,24 @@
 <template>
   <div class="home">
+     <tasks/>
     <create/>
   </div>
 </template>
 
 <script>
 import create from '../components/tasks/Createtask'
+import tasks from '../components/tasks/Tasks'
 export default {
   name: 'Home',
   components: {
-   create
+   create,
+   tasks
   },
 }
 </script>
+<style>
+.home{
+  display: flex;
+  
+}
+</style>
