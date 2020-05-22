7c01d7aeec616ef63c12d2f7d6342c8cb2b13fb7
mark odhiambo okoth <markokoth96@gmail.com>
2020-05-22 10:35:36 +0300

    create task

---
+++ src/components/tasks/Createtask.vue
@@ -0,0 +1,30 @@
+<template>
+    <div class="create">
+        <div class="container">
+
+        </div>
+    </div>
+</template>
+
+
+<script>
+export default {
+    name: 'create',
+    data(){
+        return{
+
+        }
+    }
+}
+</script>
+
+<style scoped>
+
+.container{
+    width: 33.3vw;
+    border: 1px solid white;
+    height: 100vh;
+    border-top:none;
+}
+
+</style>
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,15 +1,15 @@
 <template>
   <div class="home">
-    
+    <create/>
   </div>
 </template>
 
 <script>
-
+import create from '../components/tasks/Createtask'
 export default {
   name: 'Home',
   components: {
-   
+   create
   },
 }
 </script>
