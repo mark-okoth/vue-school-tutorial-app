acb1edfd698d32280cdcaf874edc00631d948f3a
mark odhiambo okoth <markokoth96@gmail.com>
2020-05-24 21:19:13 +0300

    videos component

--- src/components/tasks/Tasks.vue
+++ src/components/tasks/Tasks.vue
@@ -1,30 +1,30 @@
 <template>
-    <div class="tasks">
-        <div class="container">
-
+  <div class="tasks">
+    <div class="container">
+      <div id="app">
+        <div class="player-container">
+          <vue-core-video-player src="./l.mp4"></vue-core-video-player>
         </div>
+      </div>
     </div>
+  </div>
 </template>
 
 <script>
 export default {
-    data(){
-        return{
-
-        }
-    }
-}
+  data() {
+    return {};
+  }
+};
 </script>
 
 <style scoped>
-
-.container{
-    width: 33.3vw;
-    border: 1px solid white;
-    height: 100vh;
-    border-top:none;
-    border-left:none;
-    border-right: none;
+.container {
+  width: 33.3vw;
+  border: 1px solid white;
+  height: 100vh;
+  border-top: none;
+  border-left: none;
+  border-right: none;
 }
-
 </style>
