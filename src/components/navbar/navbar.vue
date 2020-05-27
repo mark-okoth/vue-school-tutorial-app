5aa943cdc4d5e823b4ba929a4ab9c5aa1f207418
mark odhiambo okoth <markokoth96@gmail.com>
2020-05-27 11:01:15 +0300

    fonts

--- src/components/navbar/navbar.vue
+++ src/components/navbar/navbar.vue
@@ -3,11 +3,11 @@
     <nav>
       <div class="nav-container">
          <ul>
-           <li><a href="">Home</a></li>
-           <li><router-link to="/about">About</router-link></li>
-           <li><a href="">Topics</a></li>
-           <li><a href="">Paths</a></li>
-           <li><a href="">Login</a></li>
+           <li><i class="fas fa-home"></i><router-link to="/">Home</router-link></li>
+           <li><i class="fas fa-user"></i><router-link to="/about">About</router-link></li>
+           <li><i class="fas fa-laptop-code"></i><a href="">Topics</a></li>
+           <li><i class="fas fa-book"></i><a href="">Paths</a></li>
+           <li><i class="fas fa-sign-in-alt"></i><a href="">Login</a></li>
          </ul>
          
 
@@ -59,6 +59,14 @@
     text-decoration: none;
     font-size: 1.3rem;
     
+  }
+
+  i{
+    margin: 0 10px;
+    font-size: 1.2rem;
+    text-align: left;
+    color: rgb(145, 145, 241);
+   
   }
   
   </style>
