fd09b62cfca6f9edf76921b08c1341b61f49ea58
mark odhiambo okoth <markokoth96@gmail.com>
2020-05-27 18:25:02 +0300

    buttons and colors

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,24 +1,48 @@
 <template>
   <div class="home">
-   <h2>About</h2>
-   <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Sed, perferendis iusto quo voluptas et dolores eos corrupti! Ex dignissimos laborum id nesciunt totam fugiat reiciendis, a sed vero rem vel, architecto itaque. Non numquam doloremque cupiditate placeat exercitationem sint aspernatur neque consequatur amet voluptatibus sed nulla ea architecto qui commodi harum, eveniet necessitatibus explicabo aliquam. Ipsam sit laboriosam nam ad voluptatum eaque quaerat. Suscipit eligendi, soluta quia accusantium reprehenderit, voluptates commodi nisi est beatae itaque perspiciatis. Numquam porro itaque suscipit perferendis animi nostrum. Doloribus, quam est! Totam cum cupiditate, molestiae alias nobis nisi ducimus quisquam praesentium eius, rerum tempore eaque.</p>
-  
-    
+    <div class="home-container">
+     
+     <p>Learn to code for free with curated video tutorials!</p>
+     <a href="">CREATE AN ACCOUNT</a>
+    </div>
   </div>
 </template>
 
 <script>
 export default {
   name: "Home",
-  components: {
-  
-  }
+  components: {}
 };
 </script>
 <style>
-.home{
-  /* background: url('../assets/pic.jpeg'); */
+.home {
+  background-image: url("../assets/pic.jpeg");
+  background-color: rgb(28, 28, 95);
+  background-blend-mode: multiply;
+  width: 100vw;
+  height: 100vh;
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-content: center;
 }
 
+.home-container{
+  margin: 20px 20vw;
+}
+.home-container p{
+  font-size: 3.5rem;
+  margin-bottom: 20px;
+}
+.home-container a{
+  text-decoration: none;
+  background:lightslategray;
+  padding: 15px 20px;
+}
 
+.home-container a:hover{
+  background: white;
+  border: ipx solid lightslategrey;
+  color: slateblue;
+}
 </style>
