d0da0a0f51ffc08a7fc44652d4c42aca9736bdd8
mark odhiambo okoth <markokoth96@gmail.com>
2020-06-02 15:37:01 +0300

    about description

--- src/views/About.vue
+++ src/views/About.vue
@@ -4,9 +4,7 @@
       <h2>About</h2>
       <p class="me">👋 Hi, I'm Mark!</p>
       <p>
-        Lorem ipsum dolor sit amet consectetur adipisicing elit. Atque ullam numquam mollitia,
-        delectus temporibus provident? Sunt voluptatum veniam voluptatem debitis, quam earum laboriosam at
-        distinctio impedit magnam repellendus placeat consequatur ea totam aliquam, praesentium assumenda ipsum eveniet!
+      I am a software developer from kenya and Manchester united fan
         Praesentium est ipsum qui nulla, labore recusandae quas tempore vel alias, odit expedita et excepturi perspiciatis.
         Distinctio corrupti natus aperiam odio, totam repellat reprehenderit molestias, atque dolorum, labore magni ex.
         Tempora similique eius soluta commodi rerum quasi assumenda fuga ad deserunt mollitia dicta modi sed tenetur
@@ -84,7 +82,7 @@ a {
   background:rgb(28, 28, 95);
   margin-right: 20px;
   border-radius: 5px;
-  transition: all 500ms ease-in-out; 
+  transition: all 500ms ease-in-out;
 }
 
 a:hover{
